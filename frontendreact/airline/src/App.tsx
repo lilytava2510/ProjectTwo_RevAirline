44a7a531a7543534002ce4a071d1b8315cf8a388
lilytava2510 <42839204+lilytava2510@users.noreply.github.com>
2022-05-28 01:47:18 -0400

    willdomoreincasejayawantedtogetstartedoncss

---
+++ frontendreact/airline/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { LoginForm } from './Components/LoginForm/LoginForm';
+
+
+function App() {
+  return (
+    <>
+   <LoginForm/>
+    </>
+  );
+}
+
+export default App;
---
+++ frontendreact/airline/src/Components/LoginForm/LoginForm.tsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import "./LoginForm.css";
+import { useDispatch } from 'react-redux';
+import { loginUser} from '../../Slices/UserSlice';
+import { AppDispatch } from '../../Store';
+
+
+export const Login: React.FC = () => {
+
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+   const dispatch: AppDispatch = useDispatch();
+
+    const handleInput = (event:React.ChangeEvent<HTMLInputElement>) => {
+        if(event.target.name === "email"){
+            setEmail(event.target.value);
+        }
+        else {
+            setPassword(event.target.value);
+        }
+        }
+
+    
+    const handleLogin = (event:React.MouseEvent<HTMLButtonElement>) => {
+        let credentials = {
+            email,
+            password
+
+        };
+
+      dispatch(loginUser(credentials));
+        
+        
+    }
+    return(
+        <div className="login">
+            <div className="text-container">
+                <h1 className="login-header">Welcome to Revature Airlines: </h1>
+                <h3 className="login-header">Please Sign In</h3>
+
+                <h2 className="login-header">Login </h2>
+            </div>
+            <form className="login-form">
+                <div className="input-div">
+                  <h4 className="input-h4">Email:</h4>
+                    <input autoComplete="off" className="login-input" type="text" name="email" placeholder="email" onChange={handleInput}/>
+                </div>
+                <div className="input-div">
+                    <h4 className="input-h4">Password:</h4>
+                    <input className="login-input" type="password" name="password" placeholder="password" onChange={handleInput}/>
+                </div>
+                <div className="input-div">
+
+                </div>
+            </form>
+                <button className="login-button" onClick={handleLogin}>Login</button>
+        </div>
