9d4c3b0815ba3155bc543a25695519b5551de085
jayab2010 <jayab2010@gmail.com>
2022-05-29 00:50:49 -0400

    added style to  loginpage and navbar

--- frontendreact/airline/src/Components/LoginForm/LoginForm.tsx
+++ frontendreact/airline/src/Components/LoginForm/LoginForm.tsx
@@ -3,6 +3,7 @@ import "./LoginForm.css";
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../../Store';
 import { loginUser } from '../../Slices/UserSlice';
+import { Navbar } from '../Navbar/Navbar';
 
 
 export const Login: React.FC = () => {
@@ -34,20 +35,22 @@ export const Login: React.FC = () => {
         
     }
     return(
+        <>
+       
         <div className="login">
             <div className="text-container">
                 <h1 className="login-header">Welcome to Revature Airlines: </h1>
                 <h3 className="login-header">Please Sign In</h3>
 
-                <h2 className="login-header">Login </h2>
+                
             </div>
             <form className="login-form">
                 <div className="input-div">
-                  <h4 className="input-h4">Email:</h4>
+                  <h4 className="input-h5">Email:</h4>
                     <input autoComplete="off" className="login-input" type="text" name="email" placeholder="email" onChange={handleInput}/>
                 </div>
                 <div className="input-div">
-                    <h4 className="input-h4">Password:</h4>
+                    <h4 className="input-h5">Password:</h4>
                     <input className="login-input" type="password" name="password" placeholder="password" onChange={handleInput}/>
                 </div>
                 <div className="input-div">
@@ -56,5 +59,6 @@ export const Login: React.FC = () => {
             </form>
                 <button className="login-button" onClick={handleLogin}>Login</button>
         </div>
+        </>
     )
 }
